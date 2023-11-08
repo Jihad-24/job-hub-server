cd64baf5791fa928ac339d5ca67a9e300d93b94d
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-09 01:51:11 +0600

    url update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        'https://job-hub-mine.netlify.app/'
+        'https://job-hub-mine.netlify.app'
     ],
     credentials: true
 }));
