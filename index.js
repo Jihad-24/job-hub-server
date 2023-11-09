276d2045bc80dc29c048315f2692fac0765951fd
Jihad Hasan <mdjihat0021@gmail.com>
2023-11-09 11:06:57 +0600

    api

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ async function run() {
             res.send(allJobs);
         })
 
-        app.get('/jobs/:id', verifyToken, async (req, res) => {
+        app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await jobsCollection.findOne(query);
